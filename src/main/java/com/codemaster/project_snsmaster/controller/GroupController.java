9134519ff8fecedf5b562d1942bd91b77e2fef1c
parkdoyoung <dy151050@gmail.com>
2024-06-05 17:32:34 +0900

    GroupController 에서 home 추가

--- src/main/java/com/codemaster/project_snsmaster/controller/GroupController.java
+++ src/main/java/com/codemaster/project_snsmaster/controller/GroupController.java
@@ -1,7 +1,17 @@
 package com.codemaster.project_snsmaster.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GroupController {
+
+
+    @RequestMapping(value = "/")//첫화면
+    public String home() {
+
+        return "home";
+    }
+
 }
