b414ce0cbc3595a589128aa93ab7bfa989baaa6c
parkdoyoung <dy151050@gmail.com>
2024-06-11 11:44:14 +0900

    그룹 만듬

--- src/main/java/com/codemaster/project_snsmaster/dao/IF_GroupDAO.java
+++ src/main/java/com/codemaster/project_snsmaster/dao/IF_GroupDAO.java
@@ -1,4 +1,27 @@
 package com.codemaster.project_snsmaster.dao;
 
+import com.codemaster.project_snsmaster.vo.GroupJoinVO;
+import com.codemaster.project_snsmaster.vo.GroupPostVO;
+import com.codemaster.project_snsmaster.vo.MemberGroupVO;
+
+import java.util.List;
+
 public interface IF_GroupDAO {
+    public void insert(GroupPostVO gpvo)throws Exception;
+    public void mginsert(MemberGroupVO mgvo)throws Exception;
+    public List<MemberGroupVO> selectAll() throws Exception;
+    public int joinsert(GroupJoinVO gjvo)throws Exception;
+
+    public List<GroupPostVO> gpselectAll(String gno)throws  Exception;
+
+
+    public void delete(int g_no)throws Exception;
+
+   public void mdelete(int gno) throws Exception;
+
+
+
+   public GroupPostVO modno(int g_no) throws Exception;
+
+   public void modupdate(GroupPostVO gpvo) throws Exception;
 }
--- src/main/java/com/codemaster/project_snsmaster/service/GroupServiceImpl.java
+++ src/main/java/com/codemaster/project_snsmaster/service/GroupServiceImpl.java
@@ -1,7 +1,66 @@
 package com.codemaster.project_snsmaster.service;
 
+import com.codemaster.project_snsmaster.dao.IF_GroupDAO;
+import com.codemaster.project_snsmaster.vo.GroupJoinVO;
+import com.codemaster.project_snsmaster.vo.GroupPostVO;
+import com.codemaster.project_snsmaster.vo.MemberGroupVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class GroupServiceImpl {
+public class GroupServiceImpl implements IF_GroupService{
+    @Autowired
+    IF_GroupDAO gdao;
+
+    @Override
+    public void insert(GroupPostVO gpvo) throws Exception {
+        gdao.insert(gpvo);
+    }
+
+    @Override
+    public void mginsert(MemberGroupVO mgvo) throws Exception {
+        gdao.mginsert(mgvo);
+    }
+
+    @Override
+    public List<MemberGroupVO> gList() throws Exception {
+
+        return gdao.selectAll();
+    }
+
+    @Override
+    public void joinsert(GroupJoinVO gjvo) throws Exception {
+        gdao.joinsert(gjvo);
+    }
+
+    @Override
+    public List<GroupPostVO> gpList(String gno) throws Exception {
+        return gdao.gpselectAll(gno);
+    }
+
+    @Override
+    public void delete(int g_no) throws Exception {
+        gdao.delete(g_no);
+    }
+
+    @Override
+    public void mdelete(int gno) throws Exception {
+        gdao.mdelete(gno);
+    }
+
+    @Override
+    public GroupPostVO modno(int g_no) throws Exception {
+
+        return gdao.modno(g_no);
+    }
+
+    @Override
+    public void modnoSaver(GroupPostVO gpvo) throws Exception {
+        gdao.modupdate(gpvo);
+    }
+
+
 }
+
